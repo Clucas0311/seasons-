93fac82f45cd7ef3d8754ba52b55b3951df851ab
Clucas0311 <clucas0311@gmail.com>
2020-11-18 13:23:49 -0500

    One Liner for intializer w/o constructor

--- src/index.js
+++ src/index.js
@@ -6,19 +6,20 @@ class App extends React.Component {
 	// used to initialize state
 	// when constructor is defined
 	// it is automatically going to be called with the props object
-	constructor(props) {
-		// you have to call a function call super with the props object
-		// super is a reference to the parent  constructor function React.Component
-		// MUST always be called, if not constructor overwrites parent constructor
-		super(props);
+	// constructor(props) {
+	// 	// you have to call a function call super with the props object
+	// 	// super is a reference to the parent  constructor function React.Component
+	// 	// MUST always be called, if not constructor overwrites parent constructor
+	// 	super(props);
 
-		//state object: it is going to contain important properties needed
-		// default the value to null - don't know the value yet/
-		// intialize state in our constructor function
-		// only time we use a direct assignment is in the constructor
-		// constructors main purpose is initailize the state object
-		this.state = { lat: null, errorMessage: '' };
-	}
+	// 	//state object: it is going to contain important properties needed
+	// 	// default the value to null - don't know the value yet/
+	// 	// intialize state in our constructor function
+	// 	// only time we use a direct assignment is in the constructor
+	// 	// constructors main purpose is initailize the state object
+	// 	this.state = { lat: null, errorMessage: '' };
+	// }
+	state = { lat: null, errotMessage: '' };
 
 	componentDidMount() {
 		window.navigator.geolocation.getCurrentPosition(
